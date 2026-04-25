f148b29f3dd1260c88d1fd876ffa7c60239641aa
nurulislamrimon <nurulislamrimon@gmail.com>
2026-04-25 13:53:18 +0600

    Module options removed

--- src/r2-storage/r2-storage.module.ts
+++ src/r2-storage/r2-storage.module.ts
@@ -1,15 +1,11 @@
+import { Module, DynamicModule } from "@nestjs/common";
+import { CloudflareService } from "./cloudflare.service";
+import { PhotoManagerService } from "./photo-manager.service";
 import {
-  Module,
-  Global,
-  ModuleMetadata,
-  Optional,
-  DynamicModule,
-} from '@nestjs/common';
-import { CloudflareService } from './cloudflare.service';
-import { PhotoManagerService } from './photo-manager.service';
-import { StorageOptions, StorageModuleOptions } from './interfaces/storage-options.interface';
-
-export const STORAGE_OPTIONS = 'STORAGE_OPTIONS';
+  StorageOptions,
+  StorageModuleOptions,
+} from "./interfaces/storage-options.interface";
+import { STORAGE_OPTIONS } from "./constants";
 
 @Module({})
 export class R2StorageModule {
@@ -39,49 +35,6 @@ export class R2StorageModule {
     return {
       module: R2StorageModule,
       global: moduleOptions.isGlobal ?? true,
-      providers: [
-        {
-          provide: STORAGE_OPTIONS,
-          useFactory: async () => {
-            const options = await optionsFactory();
-            return options;
-          },
-        },
-        CloudflareService,
-        PhotoManagerService,
-      ],
-      exports: [CloudflareService, PhotoManagerService],
-    };
-  }
-}
-
-@Global()
-@Module({})
-export class R2StorageModuleWithOptions {
-  static forRoot(
-    options: StorageOptions,
-    moduleOptions: StorageModuleOptions = {},
-  ): DynamicModule {
-    return {
-      module: R2StorageModuleWithOptions,
-      providers: [
-        {
-          provide: STORAGE_OPTIONS,
-          useValue: options,
-        },
-        CloudflareService,
-        PhotoManagerService,
-      ],
-      exports: [CloudflareService, PhotoManagerService],
-    };
-  }
-
-  static forRootAsync(
-    optionsFactory: () => Promise<StorageOptions> | StorageOptions,
-    moduleOptions: StorageModuleOptions = {},
-  ): DynamicModule {
-    return {
-      module: R2StorageModuleWithOptions,
       providers: [
         {
           provide: STORAGE_OPTIONS,
